3e7da7090b2ea7e0e43ac6b9f6a76d4cccaab109
ssamko0911 <ssamko091184@gmail.com>
2025-04-30 11:23:22 +0100

    Add previous task

--- src/task003/ZywOo.java
+++ src/task003/ZywOo.java
@@ -2,6 +2,8 @@ package task003;
 
 import java.util.Arrays;
 
+//https://www.codewars.com/kata/545afd0761aa4c3055001386
+
 public class ZywOo {
     public static int[] take(int[] arr, int n) {
         if (0 == arr.length) {
