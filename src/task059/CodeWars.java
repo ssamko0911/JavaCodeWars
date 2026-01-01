abc1190a1ed3ea986e379e493e55641015c6039f
ssamko0911 <ssamko091184@gmail.com>
2026-01-01 17:41:08 +0000

    Add task - Holiday III - Fire on the boat(l7)

---
+++ src/task059/CodeWars.java
@@ -0,0 +1,10 @@
+package task059;
+
+public class CodeWars {
+    final static private String MATCH = "Fire";
+
+    public static String fireFight(String str) {
+        return str.replace(CodeWars.MATCH, "~~");
+
+    }
+}
