915a2f56638b356d529c42b2bc4d560d04c6da40
ssamko0911 <ssamko091184@gmail.com>
2026-01-02 09:48:11 +0000

    Added task - Find numbers which are divisible by given number(l8)

---
+++ src/task072/EvenNumbers.java
@@ -0,0 +1,28 @@
+package src.task072;
+
+//https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/java
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenNumbers {
+    public static int[] divisibleBy(int[] numbers, int divider) {
+        if (divider == 0) {
+            return new int[0];
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % divider == 0) {
+                list.add(number);
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    static void main() {
+        System.out.println(Arrays.toString(divisibleBy(new int[] {1,2,3,4,5,6},2)));
+    }
+}
