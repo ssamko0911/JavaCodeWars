e1c196654cc690d91aa16fc0b78fa1e7958c9980
ssamko0911 <ssamko091184@gmail.com>
2026-01-02 10:01:51 +0000

    Added task - Duck Duck Goose(l8)

---
+++ src/task073/Kata.java
@@ -0,0 +1,9 @@
+package src.task073;
+
+public class Kata {
+    public static String duckDuckGoose(String[] players, int goose) {
+        int idx = (goose - 1) % players.length;
+
+        return players[idx];
+    }
+}
