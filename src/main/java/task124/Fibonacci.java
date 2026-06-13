3002d1c2421ebde4f667bf66d7e047ebe0bace75
ssamko0911 <ssamko091184@gmail.com>
2026-06-13 22:44:22 +0100

    Updated comment

--- src/main/java/task124/Fibonacci.java
+++ src/main/java/task124/Fibonacci.java
@@ -3,14 +3,14 @@ package task124;
 // https://www.codewars.com/kata/57a1d5ef7cb1f3db590002af/train/java
 
 public class Fibonacci {
-    public static long fib (int n){
+    public static long fib(int n) {
         long elementPosition = 1;
 
         long previousEl = 0;
         long currentEl = 1;
         long nextEl;
 
-        while(elementPosition != n){
+        while (elementPosition != n) {
             nextEl = currentEl + previousEl;
             elementPosition++;
             previousEl = currentEl;
@@ -21,15 +21,13 @@ public class Fibonacci {
     }
 }
 
-/**
- * Recursive!
- *
- * public class Fibonacci {
- * 	public static long fib (int n) {
- *     if (n <= 1) {
- *       return n;
- *     }
- * 		return fib(n - 1) + fib(n -2);
- * 	    }
- * }
- */
+//
+// Recursive!
+// public static long fib (int n) {
+//      if (n <= 1) {
+//        return n;
+//      }
+//
+//  	return fib(n - 1) + fib(n -2);
+//	    }
+// }
